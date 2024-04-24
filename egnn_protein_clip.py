3c2374a06885b1f3a2c03984b6f5a31bde65d021
Ayush Varshney <ayush.a.varshney@gmail.com>
2024-04-24 16:51:59 +0000

    add gradient caching

--- egnn_protein_clip.py
+++ egnn_protein_clip.py
@@ -1,14 +1,17 @@
+import os 
 from pathlib import Path
 import torch
 from torch import nn
 import torch.nn.functional as F
 from torch.optim import Adam
+from torch.cuda.amp import GradScaler, autocast
+from grad_cache.functional import cached, cat_input_tensor
 from transformers import EsmTokenizer, EsmModel
 from egnn_pytorch import EGNN_Network
-from modules import seed
-import os 
 from datetime import datetime
 import matplotlib.pyplot as plt
+
+from modules import seed
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -325,6 +328,80 @@ def _contrastive_loss(embedding1, embedding2):
 
 
 
+def train_gc(model, data_loader, optimizer, scaler, device, accumulated_batches=1):
+    model.train()
+    total_loss = 0
+
+    cache_x = []
+    cache_y = []
+    closures_x = []
+    closures_y = []
+
+    big_batches = 0
+    for step, sub_batch in enumerate(data_loader):
+        sequence1, sequence2 = sub_batch
+
+        seq1 = {}
+        seq1['feats'] = sequence1[0][0].to(device)
+        seq1['mask'] = sequence1[0][1].to(device)
+        seq1['adj_mat'] = _get_adj_mat(sequence1[0][1]).to(device)
+        seq1['coords'] = sequence1[1].to(device)
+        seq1['temperature'] = model.temperature
+
+        seq2 = {}
+        seq2['feats'] = sequence2[0][0].to(device)
+        seq2['mask'] = sequence2[0][1].to(device)
+        seq2['adj_mat'] = _get_adj_mat(sequence2[0][1]).to(device)
+        seq2['coords'] = sequence2[1].to(device)
+        seq2['temperature'] = model.temperature
+
+        rx, cx = _call_model_gc(model.encoder1, seq1)
+        ry, cy = _call_model_gc(model.encoder2, seq2)
+
+        cache_x.append(rx)
+        cache_y.append(ry)
+        closures_x.append(cx)
+        closures_y.append(cy)
+
+        if (step + 1) % accumulated_batches == 0:
+            big_batches += 1
+            print(step)
+            loss = _contrastive_loss_gc(cache_x, cache_y)
+            total_loss += loss.item()
+            scaler.scale(loss).backward()
+
+            for f, r in zip(closures_x, cache_x):
+                f(r)
+            for f, r in zip(closures_y, cache_y):
+                f(r)
+
+            cache_x = []
+            cache_y = []
+            closures_x = []
+            closures_y = []
+
+            scaler.step(optimizer)
+            scaler.update()
+            optimizer.zero_grad()
+
+    return total_loss / big_batches
+
+@autocast()
+def _call_model_gc(model, input):
+    return model(input)
+
+@cat_input_tensor
+@autocast()
+def _contrastive_loss_gc(x, y):
+    logits = torch.matmul(x, y.transpose(0, 1))
+    exp_logits = torch.exp(logits)
+    L_r = -torch.mean(torch.log(torch.exp(torch.diag(logits)) / torch.sum(exp_logits, dim=1)))
+    L_p = -torch.mean(torch.log(torch.exp(torch.diag(logits)) / torch.sum(exp_logits, dim=0)))
+    loss = (L_r + L_p) * 0.5
+    return loss
+
+
+
 
 
 
@@ -361,6 +438,17 @@ def _compute_embedding_cosine_similarities(model, embedding1, embedding2):
     similarity_matrix = torch.mm(embedding1, embedding2.t())
     return similarity_matrix * torch.exp(-model.temperature)
 
+def plot_loss_curves(base_path, train_losses, val_losses, train_batch_size, val_batch_size):
+    title = 'Training and Validation Loss Relative to Random'
+    plt.plot([i/-torch.tensor(1/train_batch_size).log().item() for i in train_losses], label='Train Loss')
+    plt.plot([i/-torch.tensor(1/val_batch_size).log().item() for i in val_losses], label='Validation Loss')
+    plt.title(title)
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss Relative to Random')
+    plt.legend()
+    plot_path = _save_plot(base_path)
+    print(f"{title} plot saved to {plot_path}")
+
 def _save_plot(base_path, fig_num=[1]):
     folder_path = os.path.join(base_path, "figures")
     os.makedirs(folder_path, exist_ok=True)
@@ -383,8 +471,12 @@ timestamp = datetime.now().strftime("%Y%m%d_%H%M%S_%f")
 base_path = f'{os.getcwd()}/runs/{timestamp}'
 os.makedirs(base_path, exist_ok=True)
 print(f"All run info will be saved to {base_path}")
-num_epochs = 20
+num_epochs = 25
 optimizer = Adam(trained_model.parameters(), lr=1e-3)
+training_with_grad_cache = True
+if training_with_grad_cache:
+    scaler = GradScaler()
+    accumulated_batches = 16
 
 train_losses, val_losses = [], []
 best_val_loss = float('inf')
@@ -399,16 +491,16 @@ with open(losses_save_path, 'w') as f:
     f.write('Epoch,Train Loss,Validation Loss\n')
 
     for epoch in range(num_epochs):
-        print('new epoch')
-        train_loss = train(trained_model, train_loader, optimizer, device)
+        if training_with_grad_cache:
+            train_loss = train_gc(trained_model, train_loader, optimizer, scaler, device, accumulated_batches)
+        else:
+            train_loss = train(trained_model, train_loader, optimizer, device)
         val_loss = evaluate(trained_model, val_loader, device)
 
         train_losses.append(train_loss)
         val_losses.append(val_loss)
 
         f.write(f"{epoch + 1},{train_loss:.4f},{val_loss:.4f}\n")
-        print(f"{epoch + 1},{train_loss:.4f},{val_loss:.4f}\n")
-
 
         if val_loss < best_val_loss:
             best_val_loss = val_loss
@@ -420,3 +512,10 @@ with open(losses_save_path, 'w') as f:
         plot_embedding_cosine_similarities(base_path, f"Trained Embedding Cosine Similarities on Train Set - Epoch {epoch + 1}", train_loader, best_trained_model, device)    
         plot_embedding_cosine_similarities(base_path, f"Trained Embedding Cosine Similarities on Val Set - Epoch {epoch + 1}", val_loader, best_trained_model, device)
 
+        torch.cuda.empty_cache()
+        print(f"Epoch {epoch + 1}/{num_epochs} - Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}")
+
+if training_with_grad_cache:
+    plot_loss_curves(base_path, train_losses, val_losses, batch_size*accumulated_batches, batch_size)
+else:
+    plot_loss_curves(base_path, train_losses, val_losses, batch_size, batch_size)
